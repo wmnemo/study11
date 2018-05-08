65b7a68e860f29b5632d01efd773f915a92667de
wmnemo <wmnemo@naver.com>
2018-05-08 17:25:07 +0900

    django 첫번째 커밋

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+# 글목록
 def post_list(request):
     # name = 'Django'
     # return HttpResponse('''<h1>Hello {myname}</h1>
@@ -11,4 +12,9 @@ def post_list(request):
     # QuertSet 사용해서 글목록 가져오기
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.filter(published_date__year='2018', published_date__month='05')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# 글 상세조회
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
